f40a6aaed91e9e6627fe723fbbf6422c572cb926
Patrik František Baláž <patrik.f.balaz@outlook.com>
2023-08-29 18:38:29 +0200

    Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 'use client';
-import { useUser } from '@auth0/nextjs-auth0/client';
+import Navbar from '@/components/navbar';
+import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0/client';
 
-export default function Home() {
+const Home = () => {
   const { user, error, isLoading } = useUser();
 
   if (isLoading) return <div>Loading...</div>;
@@ -9,11 +10,16 @@ export default function Home() {
 
   if (user) {
     return (
-      <div>
-        Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
-      </div>
+      <>
+        <Navbar />
+        <div className="pt-20">
+          Home
+        </div>
+      </>
     );
   }
 
   return <a href="/api/auth/login">Login</a>;
 }
+
+export default withPageAuthRequired(Home);
---
+++ src/components/navbar.tsx
@@ -0,0 +1,34 @@
+'use client';
+import Image from 'next/image'
+import logo from '../../public/tlis-WS-black.png';
+import { useUser } from '@auth0/nextjs-auth0/client';
+import { faPersonRunning, faDoorOpen, faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const Navbar = () => {
+    const { user } = useUser();
+
+    return (
+        <nav className='bg-white flex p-4 items-center justify-between fixed w-full'>
+            <span className='sm:hidden text-xl'><FontAwesomeIcon icon={faBars} /></span>
+            <span className='flex flex-row items-center gap-4'>
+                <Image
+                    src={logo}
+                    alt="Logo"
+                    height={40}
+                    priority={true}
+                />
+                <span className='hidden sm:flex sm:flex-row gap-2'><p className='font-semibold'>TLIS on you!</p>{user?.nickname}</span>
+            </span>
+            <a
+                className='flex gap-2 border p-1 rounded-md text-xl text-red-400 border-red-400 hover:text-white hover:bg-red-400'
+                href='/api/auth/logout'
+            >
+                <FontAwesomeIcon icon={faPersonRunning} />
+                <FontAwesomeIcon icon={faDoorOpen} />
+            </a>
+        </nav>
+    )
+}
+
+export default Navbar;
