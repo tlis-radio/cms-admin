8c61b88a16aefac5c10c743a4fef2d6dce60a8c4
NAN0 <135762196+ONAN0@users.noreply.github.com>
2024-02-03 17:11:15 +0100

    User list

--- src/app/management/users/page.tsx
+++ src/app/management/users/page.tsx
@@ -1,9 +1,48 @@
 'use client';
+
+import CmsApiService from '@/services/cms-api-service';
 import { withPageAuthRequired } from '@auth0/nextjs-auth0/client';
-import { FunctionComponent } from "react";
+import { useRouter } from 'next/navigation';
+import { FunctionComponent } from 'react';
+import { usePathname } from 'next/navigation'
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Pagination } from '@/models/pagination';
+import { User } from '@/models/user';
+import PaginationTable from '@/components/pagination-table';
+
+const tableHeadings = ['Obrázok','Login','Heslo','Tag',''];
 
 const Users: FunctionComponent = () => {
-    return <p>Users</p>
+    const router = useRouter();
+    const pathname = usePathname();
+
+    const dataToRowTransformationFn = (data: Pagination<User>): Array<Array<number | string | React.ReactNode>> => {
+        if (data?.results) {
+            return data.results.map((user, index) => [
+                user.nickname,
+                <FontAwesomeIcon
+                    className='cursor-pointer'
+                    key={index}
+                    icon={faPenToSquare}
+                    onClick={() => router.push(`${pathname}/form?id=${user.id}`)}
+                />
+            ]);
+        }
+
+        return [];
+    };
+
+    return (
+        <PaginationTable
+            title='Uživatelia'
+            tableHeadings={tableHeadings}
+            queryKey='showsPage'
+            queryFn={CmsApiService.User.PaginationAsync}
+            dataToRowTransformationFn={dataToRowTransformationFn}
+            maxRows={4}
+        />
+    )
 };
-  
+
 export default withPageAuthRequired(Users);
