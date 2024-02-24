721b12b8bc5000978dceee6b5e5aa89be527edeb
NAN0 <135762196+ONAN0@users.noreply.github.com>
2024-02-24 23:40:14 +0100

    table adaptation to screen size

--- src/app/management/layout.tsx
+++ src/app/management/layout.tsx
@@ -2,19 +2,15 @@
 import Navbar from '@/components/navbar';
 import { withPageAuthRequired } from '@auth0/nextjs-auth0/client';
 
-const ManagementLayout = ({
-  children,
-}: {
-  children: React.ReactNode
-}) => {
+const ManagementLayout = ({ children, }: { children: React.ReactNode }) => {
   return (
     <>
-        <Navbar />
-        <div className='flex flex-row justify-center h-full pt-[70px] pl-[50px] sm:pl-[80px] w-full mt-4'>
-          <div className='bg-white p-4 w-full sm:w-3/4 md:w-2/4 mx-1 sm:mx-0'>
-            {children}
-          </div>
+      <Navbar />
+      <div className='flex flex-row justify-center h-full pt-[70px] pl-[50px] sm:pl-[80px] w-full mt-4'>
+        <div className='bg-white p-4 w-full sm:w-3/4 md:w-2/4 mx-1 sm:mx-0'>
+          {children}
         </div>
+      </div>
     </>
   );
 };
--- src/app/management/users/page.tsx
+++ src/app/management/users/page.tsx
@@ -3,7 +3,7 @@
 import CmsApiService from '@/services/cms-api-service';
 import { withPageAuthRequired } from '@auth0/nextjs-auth0/client';
 import { useRouter } from 'next/navigation';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useState, useEffect } from 'react';
 import { usePathname } from 'next/navigation'
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -11,26 +11,70 @@ import { Pagination } from '@/models/pagination';
 import PaginationTable from '@/components/pagination-table';
 import { UserBasicInformations } from '@/models/user/user-basic-informations';
 
-const tableHeadings = ['Obrázok','Login','Meno','Priezvisko','Rola','Status',''];
+const phoneTableHeadings = ['Meno','Priezvisko','Prezývka',''];
+const normalScreenTableHeadings = ['Meno','Priezvisko','Prezývka','E-mail',''];
+const wideScreenTableHeadings = ['Meno','Priezvisko','Prezývka','E-mail','Membership','Role',''];
 
 const Users: FunctionComponent = () => {
     const router = useRouter();
     const pathname = usePathname();
+    const [screenType, setScreenType] = useState('');
+
+    const getTableHeadings = () => {
+        switch (screenType) {
+            case 'phone':
+                return phoneTableHeadings;
+            case 'normal':
+                return normalScreenTableHeadings;
+            case 'wide':
+                return wideScreenTableHeadings;
+            default:
+                return [];
+        }
+    };
+
+    useEffect(() => {
+        const handleResize = () => {
+            const width = window.innerWidth;
+            if (width <= 1440) {
+                setScreenType('phone');
+            } else if (width <= 2048) {
+                setScreenType('normal');
+            } else {
+                setScreenType('wide');
+            }
+        };
+
+        handleResize(); // Initial check
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
     const dataToRowTransformationFn = (data: Pagination<UserBasicInformations>): Array<Array<number | string | React.ReactNode>> => {
         if (data?.results) {
-            return data.results.map((user, index) => [
-                user.nickname,
-                user.email,
-                user.firstname,
-                user.lastname,
-                <FontAwesomeIcon
-                    className='cursor-pointer'
-                    key={index}
-                    icon={faPenToSquare}
-                    onClick={() => router.push(`${pathname}/form?id=${user.id}`)}
-                />
-            ]);
+            return data.results.map((user, index) => {
+                const commonData = [
+                    user.firstname,
+                    user.lastname,
+                    user.nickname,
+                ];
+
+                let additionalData: string[] = [];
+                if (screenType === 'normal') {
+                    additionalData = [user.email];
+                } else if (screenType === 'wide') {
+                    additionalData = [user.email, user.nickname, user.nickname];
+                }
+
+                return [...commonData, ...additionalData, 
+                    <FontAwesomeIcon
+                        className='cursor-pointer'
+                        key={index}
+                        icon={faPenToSquare}
+                        onClick={() => router.push(`${pathname}/form?id=${user.id}`)} 
+                    />
+                ];
+            });
         }
 
         return [];
@@ -39,7 +83,7 @@ const Users: FunctionComponent = () => {
     return (
         <PaginationTable
             title='Uživatelia'
-            tableHeadings={tableHeadings}
+            tableHeadings={getTableHeadings()}
             queryKey='usersPage'
             queryFn={CmsApiService.User.PaginationAsync}
             dataToRowTransformationFn={dataToRowTransformationFn}
